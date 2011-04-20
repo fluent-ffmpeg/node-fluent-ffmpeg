5b2bd27c49bde0e4338bdc32b2a4b382d5dbba7d
schaermu <schaermu@gmail.com>
2011-04-20 13:35:11 +0200

    fixed flashvideo preset

--- examples/thumbnails.js
+++ examples/thumbnails.js
@@ -3,7 +3,7 @@ var ffmpeg = require('../lib/fluent-ffmpeg');
 var proc = new ffmpeg('/path/to/your_movie.avi')
   // set the size of your thumbnails
   .withSize('150x100')
-  // take 5 thumbnails evenly distributed across the movie
-  .takeScreenshots(5, '/path/to/thumbnail/folder', function(err) {
-    console.log('screenshots were saved')
+  // take 2 screenshots at predefined timemarks
+  .takeScreenshots({ count: 2, timemarks: [ '00:00:02.000', '6' ] }, '/path/to/thumbnail/folder', function(err) {
+    console.log('screenshots were saved');
   });
