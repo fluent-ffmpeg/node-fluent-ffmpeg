eef4eba2405ca81d097d0c5a94dc91127aeb530c
schaermu <schaermu@gmail.com>
2012-11-28 16:45:16 +0100

    fixed env in build.bat

--- build.bat
+++ build.bat
@@ -4,4 +4,4 @@ set REPORTER="list"
 :test
 	setx NODE_ENV "test"
 	node_modules\.bin\mocha.cmd -u bdd --require should --reporter "%REPORTER%"
-	setx NODE_ENV "production"
+	setx NODE_ENV "development"
