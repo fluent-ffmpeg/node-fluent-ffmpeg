baf569bf8e8e19c72067a390dce265a75fb6ff58
Eralp Karaduman <eralpkaraduman@gmail.com>
2012-12-19 14:51:28 +0200

    removed uuid dependency

--- examples/mergeVideos.js
+++ examples/mergeVideos.js
@@ -7,17 +7,21 @@ var ffmpeg = require('../index');
  ffmpeg -i source.mp4 -qscale:v 1 intermediate2.mpg
  ffmpeg -i concat:"intermediate1.mpg|intermediate2.mpg" -c copy intermediate_all.mpg
  ffmpeg -i intermediate_all.mpg -qscale:v 2 output.mp4
+
+ Create temporary .mpg files for each video and deletes them after merge is completed.
+ These files are created by filename pattern like [videoFilename.ext].temp.mpg [outputFilename.ext].temp.merged.mp4
  */
 
 var firstFile = "title.mp4";
-var secondFile = "source.mp4"
-var outPath = "out.mp4"
-var tempFolderPath = "myTempFolder/"; // requires a temp folder to store intermediate videos, deletes them after merge is completed.
+var secondFile = "source.mp4";
+var thirdFile = "third.mov";
+var outPath = "out.mp4";
 
 var proc = new ffmpeg({source:firstFile,nolog:true})
     .mergeAdd(secondFile)
-    //.mergeAdd(thirdFile)
+    .mergeAdd(thirdFile)
     //.mergeAdd(fourthFile)
-    .mergeToFile(outPath,tempFolderPath,function(){
-        console.log('files has been merged succesfully');
+    //.mergeAdd(...)
+    .mergeToFile(outPath,function(){
+        console.log('files have been merged successfully');
     });
