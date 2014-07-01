f70e49975096fade6f35e72b82d256c3dbad225d
Nicolas Joyard <joyard.nicolas@gmail.com>
2014-07-01 10:28:26 +0200

    Update static ffmpeg URL for tests

--- tools/test-travis.sh
+++ tools/test-travis.sh
@@ -10,11 +10,11 @@ if [ "$(uname)" = "Linux" ]; then
 	# Linux
 	sudo apt-get update
 	sudo apt-get -y install wget tar bzip2 flvtool2
-	wget http://johnvansickle.com/ffmpeg/releases/ffmpeg-2.2.1-64bit-static.tar.bz2
-	tar jxf ffmpeg-2.2.1-64bit-static.tar.bz2
-	sudo cp ffmpeg-2.2.1-64bit-static/ffmpeg ffmpeg-2.2.1-64bit-static/ffprobe /usr/bin
-	export ALT_FFMPEG_PATH=$(pwd)/ffmpeg-2.2.1-64bit-static/ffmpeg
-	export ALT_FFPROBE_PATH=$(pwd)/ffmpeg-2.2.1-64bit-static/ffprobe
+	wget http://johnvansickle.com/ffmpeg/releases/ffmpeg-2.2.4-64bit-static.tar.bz2
+	tar jxf ffmpeg-2.2.4-64bit-static.tar.bz2
+	sudo cp ffmpeg-2.2.4-64bit-static/ffmpeg ffmpeg-2.2.4-64bit-static/ffprobe /usr/bin
+	export ALT_FFMPEG_PATH=$(pwd)/ffmpeg-2.2.4-64bit-static/ffmpeg
+	export ALT_FFPROBE_PATH=$(pwd)/ffmpeg-2.2.4-64bit-static/ffprobe
 else
 	# OSX
 	brew update
