ee7652ca85d5a1a807abc0fcd36ab73b8d566b92
Nick Verlinden <verlinden.nick@gmail.com>
2018-12-08 12:27:06 +0100

    Removed ffserver from test script. Tool is removed from ffmpeg 4.1.

--- tools/test-travis.sh
+++ tools/test-travis.sh
@@ -11,7 +11,7 @@ wget http://johnvansickle.com/ffmpeg/builds/ffmpeg-git-64bit-static.tar.xz
 tar xf ffmpeg-git-64bit-static.tar.xz
 
 mkdir -p $HOME/bin
-cp ffmpeg-git-*-static/{ffmpeg,ffprobe,ffserver} $HOME/bin
+cp ffmpeg-git-*-static/{ffmpeg,ffprobe} $HOME/bin
 cp ffmpeg-git-*-static/{ffmpeg,ffprobe} $(pwd)
 
 export PATH=$(pwd)/bin:$PATH
