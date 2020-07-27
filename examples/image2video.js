6ea53b11917eec0aaf3ac2e72985190b55c1263f
Martin Barker <martinbarker99@gmail.com>
2020-07-26 17:06:46 -0700

    Update image2video.js
    this code is outdated and will result in the error:
    ```index.js:132 an error happened: ffmpeg exited with code 1: Error initializing output stream 0:0 -- Error while opening encoder for output stream #0:0 - maybe incorrect parameters such as bit_rate, rate, width or height
    Conversion failed!```
    without including:
    
    //audio bitrate 
        .audioBitrate('128k')
        //video bitrate 
        .videoBitrate('8000k', true)
        //resolution
        .size('1920x1080')

--- examples/image2video.js
+++ examples/image2video.js
@@ -6,6 +6,12 @@ var proc = ffmpeg('/path/to/your_image.jpg')
   .loop(5)
   // using 25 fps
   .fps(25)
+  //audio bitrate 
+  .audioBitrate('128k')
+  //video bitrate 
+  .videoBitrate('8000k', true)
+  //resolution
+  .size('1920x1080')
   // setup event handlers
   .on('end', function() {
     console.log('file has been converted succesfully');
